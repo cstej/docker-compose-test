9a0a9ae4c13e11a2414f63c3cef45b672a0db0f6
Tejendra Kumar <34958109+cstej@users.noreply.github.com>
2024-07-22 11:25:23 +0530

    Update index.tsx

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create</title>
+        <title>Create jnj</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
